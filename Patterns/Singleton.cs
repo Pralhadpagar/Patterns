346152ee510cb1c32816918f36e9a08b505d173f
pralhadpagar <pralhadpgr86@gmail.com>
2021-06-18 23:16:34 +0530

    No thread safe singaleton pattern

--- Patterns/Singleton.cs
+++ Patterns/Singleton.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace Patterns
 {
-    class Singleton
+   public  class Singleton
     {
+        static void Main(string[] args)
+        {
+                        
+              singletonClass myclass = singletonClass.Instance;
+              singletonClass.printDetail();
+
+        }
     }
+
+
+    /// <summary>
+    /// singaleton class in sealed should be because if inheritance not possible.
+    /// </summary>
+    public sealed class singletonClass
+
+    {
+        private static int counter = 0;
+
+        // private static readonly singletonClass singletonInstance = new singletonClass();
+        private static singletonClass singletonInstance = null;
+        private singletonClass()
+        {
+            counter++;
+            Console.WriteLine("Counter is {0}", counter);
+        }
+
+        public static singletonClass Instance        
+        {
+            get
+            {
+                if(singletonInstance==null)
+                {
+
+                    singletonInstance = new singletonClass();
+                }
+               return singletonInstance ;
+            }            
+             
+        }
+
+        public static void printDetail()
+        {
+            Console.WriteLine("print");
+            Console.Read();
+        }
+
+    }
+
 }
